6c12194787986cd18a41c62a195d7046544fbf06
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2014-04-08 09:47:52 -0300

    Removed factory girl

--- lib/suspenders/app_builder.rb
+++ lib/suspenders/app_builder.rb
@@ -47,15 +47,6 @@ module Suspenders
       inject_into_class 'config/application.rb', 'Application', config
     end
 
-    def enable_factory_girl_syntax
-      copy_file 'factory_girl_syntax_rspec.rb', 'spec/support/factory_girl.rb'
-    end
-
-    def test_factories_first
-      copy_file 'factories_spec.rb', 'spec/models/factories_spec.rb'
-      append_file 'Rakefile', factories_spec_rake_task
-    end
-
     def configure_smtp
       copy_file 'smtp.rb', 'config/initializers/smtp.rb'
 
